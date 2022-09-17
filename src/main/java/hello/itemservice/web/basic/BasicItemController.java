c9c23698a62f6542f4ddbe7f55c86bcbd6fde853
JinyongHyun <wlsdyd4270@gmail.com>
2022-09-17 16:40:35 +0900

    상품등록, 상품수정(미완)

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -25,6 +24,59 @@ public class BasicItemController {
        List<Item> items= itemRepository.findAll();
        model.addAttribute("items",items);
        return "basic/items";
+    }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+    //상품등록
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+
+//    @PostMapping("/add")
+//    public String save(@RequestParam String itemName,
+//                       @RequestParam int price,
+//                       @RequestParam Integer quantity,
+//                       Model model
+//                       ){
+//                Item item = new Item();
+//                item.setItemName(itemName);
+//                item.setPrice(price);
+//                item.setQuantity(quantity);
+//
+//                itemRepository.save(item);
+//                   //아이템 넣기
+//                model.addAttribute("item", item);
+//            //상품저장 후 상세화면에서 보여주기
+//            //값들을 넘겨주기
+//        return "basic/item";
+//    }
+
+
+      //모델 사용
+      @PostMapping("/add")
+      public String addItem(@ModelAttribute("item") Item item, Model model){
+
+          itemRepository.save(item);
+          //아이템 넣기
+          model.addAttribute("item", item);
+          //상품저장 후 상세화면에서 보여주기
+          //값들을 넘겨주기
+          return "basic/item";
+      }
+
+
+      //상품수정
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){ //어떤 상품을 수정할지 id로 확인
+        Item item =itemRepository.findById(itemId); //findbyid로 id찾아서 담는다
+        model.addAttribute("item",item);
+        return "basic/editForm";
     }
 
 /**
